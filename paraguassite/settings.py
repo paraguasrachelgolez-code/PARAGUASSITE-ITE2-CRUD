b59ffbda5c9ad03c7462726b460fb2aa49ef633f
paraguasrachelgolez-code <paraguas.rachelgolez@filamer.edu.ph>
2026-05-09 21:17:01 +0800

    Polished Toast Message

--- paraguassite/settings.py
+++ paraguassite/settings.py
@@ -118,14 +118,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = '/static/'
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = [
+    BASE_DIR / 'crud' / 'static',
+]
 
+# Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 import os
